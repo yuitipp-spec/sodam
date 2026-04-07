cb90cee7d06d992d6cc6967bee5cc7631738efdc
yuitipp <yuitipp@gmail.com>
2026-04-08 01:47:45 +0900

    add static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -7,5 +8,4 @@ const nextConfig = {
     unoptimized: true,
   },
 }
-
 export default nextConfig
